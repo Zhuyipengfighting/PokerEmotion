a64ddd6811dc554d55df99c2bed5a1fe34dfaa59
gdsbapple <13790793568@stu.gdou.edu.cn>
2025-01-11 01:47:22 +0800

    feat: 项目springboot后端框架

---
+++ p_emotion_springboot/src/main/java/per/test/p_emotion_springboot/PEmotionSpringbootApplication.java
@@ -0,0 +1,13 @@
+package per.test.p_emotion_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PEmotionSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PEmotionSpringbootApplication.class, args);
+    }
+
+}
---
+++ p_emotion_springboot/src/test/java/per/test/p_emotion_springboot/PEmotionSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package per.test.p_emotion_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PEmotionSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
