c3c645f5d72fb0c13ca324799819d93bb21efb68
gdsbapple <13790793568@stu.gdou.edu.cn>
2025-02-12 15:32:09 +0800

    feat: 四大页面的构建

---
+++ p_emotion_app/app/src/main/java/com/example/pokeremotionapplication/ui/my/MyFragment.java
@@ -0,0 +1,21 @@
+package com.example.pokeremotionapplication.ui.my;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.pokeremotionapplication.R;
+
+// 个人中心
+public class MyFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+}
